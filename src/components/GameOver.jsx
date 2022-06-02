2a5e8c80fab8c0c99273b7127faaff71484f0914
Dan Ranges <danranges@gmail.com>
2022-06-02 22:31:57 +0200

    Update game over screen text

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,18 +1,30 @@
-import { Flex, Button, Heading, Text } from '@chakra-ui/react';
+import { Flex, Button, Heading, Box } from '@chakra-ui/react';
 import React from 'react';
 
 const GameOver = ({ hasWon, handleNewGame, palette }) => {
   return (
     <Flex flexDirection={'column'}>
       {hasWon ? (
-        <Heading as='h2' size='xl'>
-          Congratulations! You beat <Text borderRadius={'md'}>{palette}!</Text>
-        </Heading>
+        <Box m={'5'}>
+          <Heading as='h2' size='xl'>
+            Congratulations!
+          </Heading>
+          <Heading as='h2' size={'xl'} color={`${palette}.500`}>
+            You beat <span>{palette}!</span>
+          </Heading>
+        </Box>
       ) : (
-        <h2>You Lose</h2>
+        <Box m={'5'}>
+          <Heading as='h2' size='xl'>
+            You lost!
+          </Heading>
+          <Heading as='h2' size={'xl'}>
+            Better luck next time!
+          </Heading>
+        </Box>
       )}
       <Button onClick={handleNewGame} size={'lg'}>
-        New Game
+        {hasWon ? 'New Game' : 'Try Again'}
       </Button>
     </Flex>
   );
